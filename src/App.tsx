e8bd08255223f4e9bca6f0a997c183d2782adec1
Andrew Brantley <github@andbrant.com>
2020-09-19 13:16:20 -0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import img from "./img/the-one.gif"; 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="image_holder">
+      <img src={img} alt="Smiley face"></img>
     </div>
   );
 }
