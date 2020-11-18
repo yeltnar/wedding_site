9fe94e9fc64f6e286851237b024b62bc3925435c
Drew Brantley <brantley@us.ibm.com>
2020-11-18 14:04:32 -0600

    some progress

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -9,7 +9,7 @@ function Home(props:any){
     return (
         <div className="home_outer" onClick={onClick}>
                     <div className="leaves_img" id="leaves1">
-                        {/* <img src={leaves} className="leaves_src"></img> */}
+                        <img src={leaves} className="leaves_src"></img>
                     </div>
                     <div className="centerContentWrapper">
                         <div></div>
@@ -17,12 +17,12 @@ function Home(props:any){
                         <div></div>
                         <div></div>
                         <div className="centerContentGrid">
-                            {/* <div className="centerContent">
+                            <div className="centerContent">
                                 <div className="img_txt">November 20, 2021</div>
-                                <div className="the_one_img">
+                                <div className="the_one_img" style={{backgroundColor:"pink"}}>
                                     <img src={img} ></img>
                                 </div>
-                            </div> */}
+                            </div>
                         </div>
                         <div></div>
                         <div></div>
@@ -30,7 +30,7 @@ function Home(props:any){
                         <div></div>
                     </div>
                     <div className="leaves_img" id="leaves2">
-                        {/* <img src={leaves} className="leaves_src"></img> */}
+                        <img src={leaves} className="leaves_src"></img>
                     </div>
         </div>
     );
