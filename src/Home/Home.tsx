9f2c30d0ed7384547842d46dd8e77cfab501c7f2
Drew Brantley <brantley@us.ibm.com>
2020-11-20 12:33:26 -0600

    fair spot...used the js var

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import "./Home.css"
 import leaves from "../img/leaves and shit.png"
@@ -6,19 +6,27 @@ import img from "../img/the-one.gif";
 
 function Home(props: any) {
 
+    useEffect(()=>{
+
+        setCustomUnits();
+        window.addEventListener('resize', setCustomUnits);
+        
+        function setCustomUnits(){
+            document.documentElement.style.setProperty('--cvh', `${window.innerHeight * 0.01}px`);
+            document.documentElement.style.setProperty('--cvw', `${window.innerWidth * 0.01}px`);
+        }
+    },[]);
+
     return (
+        <>
         <div className="home_outer" onClick={onClick}>
             <div className="leaves_img" id="leaves1">
                 <img src={leaves} className="leaves_src"></img>
             </div>
             <div className="centerContentWrapper">
-                <div className="centerContentGrid">
-                    <div className="centerContent">
-                        <div className="img_txt">November 20, 2021</div>
-                            <div className="the_one_img">
-                                <img src={img} ></img>
-                            </div>
-                    </div>
+                <div className="centerContent">
+                    <div className="img_txt">November 20, 2021</div>
+                    <div className="the_one_img"><img src={img} ></img></div>
                 </div>
             </div>
             <div className="leaves_img" id="leaves2">
@@ -27,6 +35,7 @@ function Home(props: any) {
             <div className="blackBox top"></div>
             <div className="blackBox bottom"></div>
         </div>
+        </>
     );
 
     function onClick() {
