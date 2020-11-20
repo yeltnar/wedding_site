991a6471be52ec8c0a5a7c59ebf2c8dec6ace2f5
Drew Brantley <brantley@us.ibm.com>
2020-11-20 11:01:49 -0600

    some changes

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -4,40 +4,32 @@ import "./Home.css"
 import leaves from "../img/leaves and shit.png"
 import img from "../img/the-one.gif"; 
 
-function Home(props:any){
+function Home(props: any) {
 
     return (
         <div className="home_outer" onClick={onClick}>
-                    <div className="leaves_img" id="leaves1">
-                        <img src={leaves} className="leaves_src"></img>
-                    </div>
-                    <div className="centerContentWrapper">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div className="centerContentGrid">
-                            <div className="centerContent">
-                                <div className="img_txt">November 20, 2021</div>
-                                <div className="the_one_img_wrapper">
-                                    <div className="the_one_img">
-                                        <img src={img} ></img>
-                                    </div>
-                                </div>
+            <div className="leaves_img" id="leaves1">
+                <img src={leaves} className="leaves_src"></img>
+            </div>
+            <div className="centerContentWrapper">
+                <div className="centerContentGrid">
+                    <div className="centerContent">
+                        <div className="img_txt">November 20, 2021</div>
+                        <div className="the_one_img_wrapper">
+                            <div className="the_one_img">
+                                <img src={img} ></img>
                             </div>
                         </div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
                     </div>
-                    <div className="leaves_img" id="leaves2">
-                        <img src={leaves} className="leaves_src"></img>
-                    </div>
+                </div>
+            </div>
+            <div className="leaves_img" id="leaves2">
+                <img src={leaves} className="leaves_src"></img>
+            </div>
         </div>
     );
 
-    function onClick(){
+    function onClick() {
         // props.history.push("/gif");
     }
 }
