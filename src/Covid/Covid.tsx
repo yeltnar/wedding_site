ab7fcdae8a48ff28138b9befc20d40fbebacd3e6
Andrew Brantley <github@andbrant.com>
2020-12-19 10:22:10 -0600

    change covid page

--- src/Covid/Covid.tsx
+++ src/Covid/Covid.tsx
@@ -5,9 +5,13 @@ import "./Covid.css"
 function Covid(props:any){
 
     return (
-        <div className="outer" onClick={onClick}>
+        <div className="outer covid" onClick={onClick}>
             <div className="outer_content">
-                <div >No one knows what next November will look like, but we will do our best to not spread COVID further. Check here for updates!</div>
+                <div >Love is infectious. So is COVID.</div>
+                <div>
+                No one knows what next November will look like, but we will do our best to not spread COVID further. 
+                Check here for updates!
+                </div>
             </div>
         </div>
     );
