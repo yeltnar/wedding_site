81101ad99adb4361545bcfb36fd185bbdefec839
Andrew Brantley <github@andbrant.com>
2020-12-19 13:05:20 -0600

    quick deploy

---
+++ docs/precache-manifest.ebf3096cd7505c4e06984977250e3465.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f42b5d6b6cf6eadc6a4715fda7aea8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc7a966bc78bb0b0835e",
+    "url": "/static/css/main.d8e20295.chunk.css"
+  },
+  {
+    "revision": "65a403985ff32962342f",
+    "url": "/static/js/2.b5fd623f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b5fd623f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7a966bc78bb0b0835e",
+    "url": "/static/js/main.3272b003.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2f85bed805c0456ffcf6cd2fe658d022",
+    "url": "/static/media/leaves and shit.2f85bed8.png"
+  },
+  {
+    "revision": "2354f916ad8c0b14f463d60194a9c43e",
+    "url": "/static/media/the-one.2354f916.gif"
+  }
+]);
