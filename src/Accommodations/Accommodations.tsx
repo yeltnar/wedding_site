ee6775d0b3862ea37878b4443f72ea1f08b54579
Andrew Brantley <github@andbrant.com>
2021-04-04 10:03:25 -0500

    add hotel link

--- src/Accommodations/Accommodations.tsx
+++ src/Accommodations/Accommodations.tsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 
 import "./Accommodations.css";
 
+const hotel_link = "https://hamptoninn.hilton.com/en/hp/groups/personalized/D/DALMYHX-SBB-20211119/index.jhtml?WT.mc_id=POG";
+
 function Home(props: any) {
 
     return (
         <>
             <div className="centerContent home">
-                <div className="txt">Stay in McKinney, yo</div>
+                <div className="txt">Make a reservation at <a href={hotel_link}>Hampton Inn &amp Suites McKinney</a></div>
             </div>
         </>
     );
