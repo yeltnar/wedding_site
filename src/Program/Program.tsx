10cb4debb42222f75989bf5f676f5e858b50e907
Andrew Brantley <github@andbrant.com>
2021-08-21 20:56:05 -0500

    update leave warning

--- src/Program/Program.tsx
+++ src/Program/Program.tsx
@@ -41,7 +41,7 @@ function Program(props:any){
             time:"10:00p",
             title:"Exit",
             icon:"/icons/JUST MARRIED.svg",
-            extra:"Please note, Uber and Lyft will not be able to pick you up from Bethel Rock. Please be sure to arrange your transportation to and from the venue prior to the event."
+            extra:"Please note, Uber and Lyft will not be able to pick up guests from Bethel Rock. Please be sure to arrange transportation to and from the venue prior to the event."
         },
     ]
 
